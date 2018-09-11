7c6d70d7a6551538dac0bbcfd2182d84d45462d5
Graham Gilbert <graham.gilbert@airbnb.com>
2018-09-11 11:11:23 -0700

    Fix the alarm randomizer

--- background.js
+++ background.js
@@ -1,12 +1,16 @@
-// alarmPeriod = Math.floor(Math.random() * 29);
+var min=20;
+var max=30;
+var alarmPeriod = Math.floor(Math.random() * (+max - +min)) + +min;
 
 chrome.alarms.create('salAlarm', {
     delayInMinutes: 1,
-    periodInMinutes: 30
+    periodInMinutes: alarmPeriod
 });
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
     if (alarm.name === 'salAlarm') {
+        console.log('alarm!')
+        console.log(alarmPeriod)
        main();
     }
 });
